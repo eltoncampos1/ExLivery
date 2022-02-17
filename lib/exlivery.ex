5f3aae74391f3ac88c7da5731b357e3c4c503430
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-17 11:24:02 -0300

    create user struct

---
+++ lib/exlivery.ex
@@ -0,0 +1,9 @@
+defmodule Exlivery do
+  @moduledoc """
+  Documentation for `Exlivery`.
+  """
+
+  def hello do
+    :world
+  end
+end
---
+++ lib/users/user.ex
@@ -0,0 +1,12 @@
+defmodule Exlivery.Users.User do
+  @keys [:name, :email, :cpf, :age]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, cpf, age) when age >= 18 do
+    {:ok, %__MODULE__{name: name, email: email, cpf: cpf, age: age}}
+  end
+
+  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
+end
