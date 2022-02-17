a583e6b047867b560e6f38ef8100318b1582af9a
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-17 15:03:01 -0300

    add some validations in order and user

--- lib/orders/order.ex
+++ lib/orders/order.ex
@@ -4,8 +4,29 @@ defmodule Exlivery.Orders.Order do
 
   defstruct @keys
 
-  def build()  do
-    {:ok, %__MODULE__{user_cpf: nil, delivery_address: nil, items: nil, total_price: nil}}
+  alias Exlivery.Users.User
+  alias Exlivery.Orders.Item
+
+  def build(%User{cpf: cpf, address: address}, [%Item{} | _items] = items)  do
+    {:ok,
+      %__MODULE__{
+        user_cpf: cpf,
+        delivery_address: address,
+        items: items,
+        total_price: calculate_total_price(items)
+    }}
   end
 
+  def build(_user, _items), do: {:error, "Invalid Params"}
+
+  defp calculate_total_price(items) do
+    items
+    |> Enum.reduce(Decimal.new("0.00"), &sum_prices(&1, &2))
+  end
+
+  defp sum_prices(%Item{unity_price: price, quantity: quantity}, acc) do
+    price
+    |> Decimal.mult(quantity)
+    |> Decimal.add(acc)
+  end
 end
--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,12 +1,12 @@
 defmodule Exlivery.Users.User do
-  @keys [:name, :email, :cpf, :age]
+  @keys [:name, :email, :cpf, :age, :address]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, cpf, age) when age >= 18 do
-    {:ok, %__MODULE__{name: name, email: email, cpf: cpf, age: age}}
+  def build(name, email, cpf, age, address) when age >= 18 do
+    {:ok, %__MODULE__{name: name, email: email, cpf: cpf, age: age, address: address}}
   end
 
-  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
+  def build(_name, _email, _cpf, _age,_address), do: {:error, "Invalid parameters"}
 end
