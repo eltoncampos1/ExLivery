fe77a6aa313db919997afa9af112c900a340457e
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-17 19:59:33 -0300

    defdelegate orders

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
-  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateOrder
   alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Orders.Agent, as: OrderAgent
 
   def start_agents do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrder, as: :call
 end
