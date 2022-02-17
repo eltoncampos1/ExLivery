bd4ad4e1fa31dd0fb5549ceab95218d768f0eb52
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-17 17:46:27 -0300

    create order factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,15 +2,15 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
-        name: "user_name",
-        email: "email@valid.com",
-        cpf: "12345678900",
-        age: 20,
-        address: "rua valida, 0"
+      name: "user_name",
+      email: "email@valid.com",
+      cpf: "12345678900",
+      age: 20,
+      address: "rua valida, 0"
     }
   end
 
@@ -20,6 +20,28 @@ defmodule Exlivery.Factory do
       category: :pizza,
       unity_price: Decimal.new("30"),
       quantity: 1
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "rua valida, 0",
+      items: [
+        %Item{
+          category: :pizza,
+          description: "Pizza",
+          quantity: 1,
+          unity_price: Decimal.new("30")
+        },
+        %Item{
+          category: :japonesa,
+          description: "temaki",
+          quantity: 2,
+          unity_price: Decimal.new("15")
+        }
+      ],
+      total_price: Decimal.new("60.00"),
+      user_cpf: "12345678900"
     }
   end
 end
